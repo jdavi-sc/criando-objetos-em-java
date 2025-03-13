bf534557f284bded62aafdacc6f1b63a2ca5e4d4
José Davi <josedscosta2005@gmail.com>
2025-03-13 15:58:28 -0300

    Criado Carro.java e Main.java, pendente fazer o README

---
+++ projeto/Carro.java
@@ -0,0 +1,13 @@
+package criandoobjetos;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    String cor;
+    String combustivel;
+
+    void descricao() {
+        System.out.println("Marca: " + marca + "\n" + "Modelo: " + modelo + "\n" + "Cor: " + cor + "\n" + "Combustivel: " + combustivel);
+    }
+}
---
+++ projeto/Main.java
@@ -0,0 +1,15 @@
+package criandoobjetos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Carro carro1 = new Carro();
+        
+        carro1.marca = "BMW";
+        carro1.modelo = "I7";
+        carro1.cor = "branca";
+        carro1.combustivel = "eletrico";
+        carro1.descricao();
+
+    }
+}
